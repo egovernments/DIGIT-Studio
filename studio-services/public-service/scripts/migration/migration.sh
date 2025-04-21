c215cfb8d6bf83f9b44c70d1e1e7c71522ff4300
debasishchakraborty-egovt <debasish@egovernmets.org>
2025-04-21 17:16:02 +0530

    Added changes for Dockerfile

--- studio-services/public-service/scripts/migration/migration.sh
+++ studio-services/public-service/scripts/migration/migration.sh
@@ -1,16 +1,13 @@
 #!/bin/sh
 
-# Load environment variables from .env file
-set -a
-. ./env/.env
-set +a
-
 # Run Flyway using uppercase environment variable names
 flyway \
-  -url="$FLYWAY_URL" \
+  -url="$DB_URL" \
+  -table ="$SCHEMA_TABLE"  \
   -user="$FLYWAY_USER" \
   -password="$FLYWAY_PASSWORD" \
   -schemas="$FLYWAY_SCHEMAS" \
   -locations="$FLYWAY_LOCATIONS" \
   -baselineOnMigrate=true \
+  -outOfOrder=true \
   migrate
