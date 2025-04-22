f14edf61ace5976470058a855abc7961fdcb5c27
debasishchakraborty-egovt <debasish@egovernmets.org>
2025-04-22 15:19:47 +0530

    Added changes for migrate.sh

--- studio-services/public-service/scripts/migration/migration.sh
+++ studio-services/public-service/scripts/migration/migration.sh
@@ -1,13 +1,9 @@
 #!/bin/sh
 
 # Run Flyway using uppercase environment variable names
-flyway \
-  -url="$DB_URL" \
-  -table="$SCHEMA_TABLE"  \
-  -user="$FLYWAY_USER" \
-  -password="$FLYWAY_PASSWORD" \
-  -schemas="$FLYWAY_SCHEMAS" \
-  -locations="$FLYWAY_LOCATIONS" \
-  -baselineOnMigrate=true \
-  -outOfOrder=true \
-  migrate
+flyway -url=$DB_URL -table=$SCHEMA_TABLE -user=$FLYWAY_USER -password=$FLYWAY_PASSWORD -locations=$FLYWAY_LOCATIONS -baselineOnMigrate=true   -outOfOrder=true migrate
+
+
+
+
+
