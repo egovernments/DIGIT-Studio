c737bd2a8cac4180bce98b43e87401c71e8d0b4c
debasishchakraborty-egovt <debasish@egovernmets.org>
2025-04-24 16:49:10 +0530

    Added changes for workflow integration

--- studio-services/public-service/scripts/migration/migration.sh
+++ studio-services/public-service/scripts/migration/migration.sh
@@ -2,8 +2,3 @@
 
 # Run Flyway using uppercase environment variable names
 flyway -url=$DB_URL -table=$SCHEMA_TABLE -user=$FLYWAY_USER -password=$FLYWAY_PASSWORD -locations=$FLYWAY_LOCATIONS -baselineOnMigrate=true   -outOfOrder=true migrate
-
-
-
-
-
