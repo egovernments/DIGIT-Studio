af52207b17ee21ee61512bda9290f73f4b056ae8
Ajinkya Pande <ajinkya.pande@tekditechnologies.com>
2025-05-12 16:23:42 +0530

    worked on navbar css costomization

--- frontend/web/micro-ui-internals/packages/modules/core/src/components/TopBarSideBar/TopBar.js
+++ frontend/web/micro-ui-internals/packages/modules/core/src/components/TopBarSideBar/TopBar.js
@@ -101,7 +101,7 @@ const TopBar = ({
   return (
     <div className="topbar">
       {mobileView ? <Hamburger handleClick={toggleSidebar} color="#9E9E9E" /> : null}
-      <img className="city" src={cityDetails?.logoId } />
+      <img className="city" src={loggedin ? cityDetails?.logoId : stateInfo?.statelogo} />
       <span style={{ display: "flex", alignItems: "center", justifyContent: "space-between", width: "100%" }}>
         {loggedin &&
           (cityDetails?.city?.ulbGrade ? (
